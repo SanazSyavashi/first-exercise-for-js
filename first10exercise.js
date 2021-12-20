61eb5ce7cb9e2f57f957b9fc77672056beacdc8f
SanazSyavashi <94061317+SanazSyavashi@users.noreply.github.com>
2021-12-20 18:11:49 +0330

    Update first10exercise.js

--- first10exercise.js
+++ first10exercise.js
@@ -108,3 +108,84 @@ function computeSum(inputNum1, inputNum2) {
     }
     return inputNum1 + inputNum2;
 }
+//11
+//Write a JavaScript program to compute the absolute difference between a specified number and 19. Returns triple their absolute difference if the specified number is greater than 19
+function computeDefference(inputNum) {
+    if (inputNum > 19) {
+        return (inputNum - 19) * 3;
+    }
+    return 19 - inputNum;
+}
+//12
+// Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50
+function checkGivenNum(num1, num2) {
+    if ((num1 === 50) || (num2 === 50) || (num1 + num2 === 50)) {
+        return true;
+    }
+    return false;
+}
+//13
+//Write a JavaScript program to create a new string adding "Py" in front of a given string. If the given string begins with "Py" then return the original string
+function addPy(inputString) {
+    let firstsilab = inputString.slice(0, 2);
+    if (firstsilab === "py") {
+        return inputString;
+    }
+    return `Py${inputString}`;
+}
+//14
+// Write a JavaScript program to remove a character at the specified position of a given string and return the new string.
+function removeCharAtPosition(inputString, inputPosition) {
+    return inputString.substring(0, inputPosition) + inputString.substring(inputPosition + 1);
+}
+//15
+//Write a JavaScript program to create a new string from a given string changing the position of first and last characters. The string length must be greater than or equal to 1
+function createNewStringFromInputString(inputString) {
+    var result = inputString.charAt(inputString.length - 1) + inputString.substring(1, inputString.length - 1) + inputString.charAt(0);
+    return result;
+}
+//16
+//Write a JavaScript program to create a new string from a given string with the first character of the given string added at the front and back.
+function front_back(inputString) {
+    let firstChar = inputString.charAt(0);
+    let result = `${firstChar}${inputString}${firstChar}`
+    return result;
+}
+//17
+//Write a JavaScript program to check whether a given positive number is a multiple of 3 or a multiple of 7
+function test3_7(inputNum) {
+    if ((inputNum % 3 === 0) || (inputNum % 7 === 0)) {
+        return true;
+    }
+    return false;
+}
+//18
+//Write a JavaScript program to create a new string from a given string taking the last 3 characters and added at both the front and back. The string length must be 3 or more
+function front_back3(inputString) {
+    if (inputString.length >= 3) {
+        let last3Char = inputString.substring(inputString.length - 3);
+        let result = `${last3Char}${inputString}${last3Char}`;
+        return result;
+    }
+    return "Wrong input!"
+}
+//19
+// Write a JavaScript program to check whether a string starts with 'Java' and false otherwise.
+function start_spec_str(inputString) {
+    if (inputString.length < 4) {
+        return false;
+    }
+    let first4Char = inputString.slice(0, 4);
+    if (first4Char === "Java") {
+        return true;
+    }
+    return false;
+}
+//20
+// Write a JavaScript program to check whether two given integer values are in the range 50..99 (inclusive). Return true if either of them are in the said range.
+function check_numbers(x, y) {
+    if ((x >= 50 && x <= 99) || (y >= 50 && y <= 99)) {
+        return true;
+    }
+    return false;
+}
